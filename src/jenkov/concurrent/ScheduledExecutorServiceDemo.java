e018ff156c9b21e8dcaa3cef5bcc0b5953d0b9e6
vonzhou <vonzhou@163.com>
2014-09-11 15:36:13 +0800

    ScheduledExcutorService

---
+++ src/jenkov/concurrent/ScheduledExecutorServiceDemo.java
@@ -0,0 +1,39 @@
+package jenkov.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorServiceDemo {
+	public static void main(String[] args) {
+		ScheduledExecutorService scheduledExecutorService =
+		        Executors.newScheduledThreadPool(5);
+
+		ScheduledFuture<Object> scheduledFuture =
+		    scheduledExecutorService.schedule(new Callable<Object>() {
+		        public Object call() throws Exception {
+		            System.out.println("Executed!");
+		            return "Called!";
+		        }
+		    },
+		    5,
+		    TimeUnit.SECONDS); // 5 秒后执行
+		
+		System.out.println("in main ");
+		
+		try {
+			//没有执行完成前，这里会阻塞？？
+			System.out.println("result = " + scheduledFuture.get());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		
+		scheduledExecutorService.shutdown();
+	}
+
+}
