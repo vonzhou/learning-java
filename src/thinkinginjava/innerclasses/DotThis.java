c9e3de57cdccd5d17e78237bdc3a3d5e3ad2cf0a
vonzhou <vonzhou@163.com>
2014-09-18 15:44:21 +0800

    Java内部类

---
+++ src/thinkinginjava/innerclasses/DotThis.java
@@ -0,0 +1,28 @@
+package thinkinginjava.innerclasses;
+//: innerclasses/DotThis.java
+// Qualifying access to the outer-class object.
+
+public class DotThis {
+  void f() { System.out.println("DotThis.f()"); }
+  
+  public class Inner {
+    public DotThis outer() {
+      return DotThis.this;
+      // A plain "this" would be Inner's "this"
+    }
+  }
+  
+  public Inner inner() {
+	  return new Inner(); 
+  }
+  
+  public static void main(String[] args) {
+    DotThis dt = new DotThis();
+    DotThis.Inner dti = dt.inner();
+    dti.outer().f();
+  }
+} 
+
+
+
+
