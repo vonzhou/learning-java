2e6b93ab3cc2083280b1f17016f93f45bbbefaa8
vonzhou <vonzhou@163.com>
2014-11-01 23:26:36 +0800

    泛型擦除

---
+++ src/thinkinginjava/generics/ErasedTypeEquivalence.java
@@ -0,0 +1,11 @@
+package thinkinginjava.generics;
+
+import java.util.ArrayList;
+
+public class ErasedTypeEquivalence {
+	public static void main(String[] args) {
+		Class c1 = new ArrayList<String>().getClass();
+		Class c2 = new ArrayList<Integer>().getClass();
+		System.out.println(c1 == c2);
+	}
+} 
