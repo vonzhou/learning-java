b10ea20a8c44608812725aae426efa6d10479820
vonzhou <vonzhou@163.com>
2014-11-01 12:58:33 +0800

    类型参数推导

--- src/thinkinginjava/generics/GenericMethods.java
+++ src/thinkinginjava/generics/GenericMethods.java
@@ -1,6 +1,7 @@
 package thinkinginjava.generics;
 
 public class GenericMethods {
+	
 	public <T> void f(T x) {
 		System.out.println(x.getClass().getName());
 		System.out.println(x.getClass().getSimpleName());
@@ -8,7 +9,7 @@ public class GenericMethods {
 
 	public static void main(String[] args) {
 		GenericMethods gm = new GenericMethods();
-		gm.f("");
+		gm.f("");  // 可以传入不同的类型，好像多次重载过 ? 
 		gm.f(1);
 		gm.f(1.0);
 		gm.f(1.0F);
---
+++ src/thinkinginjava/util/TypeInferenceTest.java
@@ -0,0 +1,12 @@
+package thinkinginjava.util;
+
+public class TypeInferenceTest {
+	public static void f(String s){
+		System.out.println("string : " + s);
+	}
+	
+	public static void f(int i){
+		System.out.println("int : " + i);
+	}
+
+}
