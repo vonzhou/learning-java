6172da7b0e92f01e604697e7baf080527f202156
vonzhou <vonzhou@163.com>
2014-11-03 09:40:33 +0800

    返回泛型有好处

---
+++ src/thinkinginjava/generics/ReturnGenericType.java
@@ -0,0 +1,13 @@
+package thinkinginjava.generics;
+
+class ReturnGenericType<T extends HasF> {
+	private T obj;
+
+	public ReturnGenericType(T x) {
+		obj = x;
+	}
+
+	public T get() {
+		return obj;
+	}
+} // /:~
