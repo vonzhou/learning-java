0d32514e893727042dfaa273ed8f9148b0af36d2
vonzhou <vonzhou@163.com>
2014-11-03 20:12:12 +0800

    自我限定 与 协变参数

---
+++ src/thinkinginjava/generics/OrdinaryArguments.java
@@ -0,0 +1,24 @@
+package thinkinginjava.generics;
+
+class OrdinarySetter {
+	void set(Base base) {
+		System.out.println("OrdinarySetter.set(Base)");
+	}
+}
+
+class DerivedSetter extends OrdinarySetter {
+	void set(Derived derived) {
+		System.out.println("DerivedSetter.set(Derived)");
+	}
+}
+
+public class OrdinaryArguments {
+	public static void main(String[] args) {
+		Base base = new Base();
+		Derived derived = new Derived();
+		DerivedSetter ds = new DerivedSetter();
+		
+		ds.set(derived);
+		ds.set(base); // Compiles: overloaded, not overridden!
+	}
+} 
