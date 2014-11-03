a045e5df690934552e24fbdbec442a617ed1b0d1
vonzhou <vonzhou@163.com>
2014-11-03 10:32:45 +0800

    模板方法模式，创建泛型实例

---
+++ src/thinkinginjava/generics/CreatorGenericMy.java
@@ -0,0 +1,15 @@
+package thinkinginjava.generics;
+
+
+class ABC<X> {
+	X create() {
+		// return new X();  // cannot
+		return null;
+	}
+
+}
+
+public class CreatorGenericMy {
+	public static void main(String[] args) {
+	}
+} 
