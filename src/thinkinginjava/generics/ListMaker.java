19f138045fc2abb9916174c8397bc4b47d7de710
vonzhou <vonzhou@163.com>
2014-11-03 09:57:16 +0800

    Arrat.newInstance 和 ArrayList<T>

---
+++ src/thinkinginjava/generics/ListMaker.java
@@ -0,0 +1,14 @@
+package thinkinginjava.generics;
+
+import java.util.*;
+
+public class ListMaker<T> {
+	List<T> create() {
+		return new ArrayList<T>();
+	}
+
+	public static void main(String[] args) {
+		ListMaker<String> stringMaker = new ListMaker<String>();
+		List<String> stringList = stringMaker.create();
+	}
+} // /:~
