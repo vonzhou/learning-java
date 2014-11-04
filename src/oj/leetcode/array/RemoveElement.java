78d66156ee7a31b204d8e6bd0ab1f9f2ad38794f
vonzhou <vonzhou@163.com>
2014-11-04 14:46:48 +0800

    Remove Element

--- src/oj/leetcode/array/RemoveElement.java
+++ src/oj/leetcode/array/RemoveElement.java
@@ -1,4 +1,4 @@
-package oj.leetcode;
+package oj.leetcode.array;
 
 
 /*
@@ -6,6 +6,8 @@ package oj.leetcode;
  * in place and return the new length.
  * The order of elements can be changed. It doesn't matter what you 
  * leave beyond the new length.
+ * 
+ * 
  */
 public class RemoveElement {
 	public int removeElement(int[] A, int elem) {
