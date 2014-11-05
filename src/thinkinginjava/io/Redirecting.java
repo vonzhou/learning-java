018c97ffac384bae519375d26dae9ff6aebc5b76
vonzhou <vonzhou@163.com>
2014-11-05 20:31:17 +0800

    标准IO重定向

---
+++ src/thinkinginjava/io/Redirecting.java
@@ -0,0 +1,26 @@
+package thinkinginjava.io;
+
+// Demonstrates standard I/O redirection.
+import java.io.*;
+
+// ÔÚ UnixÖÐÊÇ dup2()
+public class Redirecting {
+	public static void main(String[] args) throws IOException {
+		PrintStream console = System.out;
+		BufferedInputStream in = new BufferedInputStream(new FileInputStream(
+				"C:/data/hello.txt"));
+		PrintStream out = new PrintStream(new BufferedOutputStream(
+				new FileOutputStream("C:/data/test.out")));
+		
+		System.setIn(in);
+		System.setOut(out);
+		System.setErr(out);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while ((s = br.readLine()) != null)
+			System.out.println(s);
+		out.close(); // Remember this!
+		
+		System.setOut(console);
+	}
+} 
