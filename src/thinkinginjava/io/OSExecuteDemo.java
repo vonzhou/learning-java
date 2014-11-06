27a1d1540b210678eca29790dbd689273f934f43
vonzhou <vonzhou@163.com>
2014-11-06 20:15:32 +0800

    Java中进程控制

---
+++ src/thinkinginjava/io/OSExecuteDemo.java
@@ -0,0 +1,10 @@
+package thinkinginjava.io;
+
+// Demonstrates standard I/O redirection.
+import thinkinginjava.util.OSExecute;
+
+public class OSExecuteDemo {
+	public static void main(String[] args) {
+		OSExecute.command("java -version");
+	}
+} 
---
+++ src/thinkinginjava/util/OSExecuteException.java
@@ -0,0 +1,7 @@
+package thinkinginjava.util;
+
+public class OSExecuteException extends RuntimeException {
+	public OSExecuteException(String why) {
+		super(why);
+	}
+} 
