c20bff6a59329452683a2de296e6023904d23e32
vonzhou <vonzhou@163.com>
2014-11-07 16:49:07 +0800

    判断字符串是否回文

---
+++ src/oj/leetcode/string/ValidPalindrome.java
@@ -0,0 +1,51 @@
+package oj.leetcode.string;
+
+/*
+ * Given a string, determine if it is a palindrome, considering only alphanumeric
+ *  characters and ignoring cases.
+ *  
+ *  For example,
+ *  "A man, a plan, a canal: Panama" is a palindrome.
+ *  "race a car" is not a palindrome.
+ *  
+ *  Note:
+ *  Have you consider that the string might be empty? 
+ *  This is a good question to ask during an interview.
+ *  
+ *  For the purpose of this problem, we define empty string as valid palindrome.
+ */
+public class ValidPalindrome {
+	public static boolean isPalindrome(String s) {
+		if(s == null)
+			return false;
+		s = s.replace(" ", "");
+		// ¿Õ´®ÊÇ»ØÎÄ
+		if(s.equals(""))
+			return true;
+		
+		char[] cs = s.toCharArray();
+		//we just need alphanumeric
+		int index = 0;
+		for(int i = 0; i < cs.length; i++){
+			if(Character.isAlphabetic(cs[i]) || Character.isDigit(cs[i]))
+				cs[index++] = cs[i];
+		}
+		int left = 0, right = index - 1;
+		while(left < right){
+			char a = Character.toLowerCase(cs[left]);
+			char b = Character.toLowerCase(cs[right]);
+			if(a != b)
+				return false;
+			left ++;
+			right --;
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String x = "A man, a plan, a canal: Panama";
+		System.out.println(isPalindrome(x));
+		System.out.println(Character.toLowerCase('2'));
+	}
+}
