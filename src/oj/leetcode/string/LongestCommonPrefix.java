1d6935b0a43680db43ac2aef3f41577ae2c57704
vonzhou <vonzhou@163.com>
2014-11-17 16:24:52 +0800

    LCP 最长公共前缀 有没有更好的方法？

---
+++ src/oj/leetcode/string/LongestCommonPrefix.java
@@ -0,0 +1,43 @@
+package oj.leetcode.string;
+
+/*
+ * Write a function to find the longest common prefix 
+ * string amongst an array of strings.
+ */
+public class LongestCommonPrefix {
+	
+	/*
+	 * native way to scan
+	 */
+	public static String longestCommonPrefix(String[] strs) {
+		if(strs == null || strs.length == 0)
+			return "";
+		// 空数组要单独考虑 strs = {};
+		if(strs.length == 1)
+			return strs[0];
+		
+		// to look up the min len
+		int min = Integer.MAX_VALUE;
+		for(String s : strs)
+			if(s.length() < min)
+				min = s.length();
+		if(min == 0)
+			return "";
+		//再次强调break只能中断一层循环
+		int i=0;
+		loop:
+		for(i=0; i < min; i++){
+			char c = strs[0].charAt(i);
+			for(int j=1; j < strs.length; j++)
+				if(strs[j].charAt(i) != c)
+					break loop;
+		}
+		
+		return strs[0].substring(0,i);
+
+	}
+	public static void main(String[] args) {
+		String ss[] = {};
+		System.out.println(longestCommonPrefix(ss));
+	}
+}
