eb71e22fd273b930d2947b062fef0690f32171b6
vonzhou <vonzhou@163.com>
2015-03-03 14:10:41 +0800

    substring search

---
+++ src/algrithm/sedgewick/strings/substringsearch/BoyerMoore.java
@@ -0,0 +1,117 @@
+package algrithm.sedgewick.strings.substringsearch;
+
+import algrithm.sedgewick.fundamental.programmodel.StdOut;
+
+public class BoyerMoore {
+    private final int R;     // the radix
+    private int[] right;     // the bad-character skip array
+
+    private char[] pattern;  // store the pattern as a character array
+    private String pat;      // or as a string
+
+    // pattern provided as a string
+    public BoyerMoore(String pat) {
+        this.R = 256;
+        this.pat = pat;
+
+        // position of rightmost occurrence of c in the pattern
+        right = new int[R];
+        for (int c = 0; c < R; c++)
+            right[c] = -1;
+        for (int j = 0; j < pat.length(); j++)
+            right[pat.charAt(j)] = j;
+    }
+
+    // pattern provided as a character array
+    public BoyerMoore(char[] pattern, int R) {
+        this.R = R;
+        this.pattern = new char[pattern.length];
+        for (int j = 0; j < pattern.length; j++)
+            this.pattern[j] = pattern[j];
+
+        // position of rightmost occurrence of c in the pattern
+        right = new int[R];
+        for (int c = 0; c < R; c++)
+            right[c] = -1;
+        for (int j = 0; j < pattern.length; j++)
+            right[pattern[j]] = j;
+    }
+
+    // return offset of first match; N if no match
+    public int search(String txt) {
+        int M = pat.length();
+        int N = txt.length();
+        int skip;
+        for (int i = 0; i <= N - M; i += skip) {
+            skip = 0;
+            for (int j = M-1; j >= 0; j--) {
+                if (pat.charAt(j) != txt.charAt(i+j)) {
+                    skip = Math.max(1, j - right[txt.charAt(i+j)]);
+                    break;
+                }
+            }
+            if (skip == 0) return i;    // found
+        }
+        return N;                       // not found
+    }
+
+
+    // return offset of first match; N if no match
+    public int search(char[] text) {
+        int M = pattern.length;
+        int N = text.length;
+        int skip;
+        for (int i = 0; i <= N - M; i += skip) {
+            skip = 0;
+            for (int j = M-1; j >= 0; j--) {
+                if (pattern[j] != text[i+j]) {
+                    skip = Math.max(1, j - right[text[i+j]]);
+                    break;
+                }
+            }
+            if (skip == 0) return i;    // found
+        }
+        return N;                       // not found
+    }
+
+    public static void test1(){
+    	String pat = "NEEDLE";
+        String txt = "FINDINAHAYSTACKNEEDLEINA";
+        BoyerMoore boyermoore1 = new BoyerMoore(pat);
+        int offset1 = boyermoore1.search(txt);
+        System.out.println(txt);
+        for (int i = 0; i < offset1; i++)
+        	System.out.print(" ");
+        System.out.println(pat);
+    }
+
+
+    // test client
+    public static void main(String[] args) {
+    	test1();
+    	/*
+        String pat = args[0];
+        String txt = args[1];
+        char[] pattern = pat.toCharArray();
+        char[] text    = txt.toCharArray();
+
+        BoyerMoore boyermoore1 = new BoyerMoore(pat);
+        BoyerMoore boyermoore2 = new BoyerMoore(pattern, 256);
+        int offset1 = boyermoore1.search(txt);
+        int offset2 = boyermoore2.search(text);
+
+        // print results
+        StdOut.println("text:    " + txt);
+
+        StdOut.print("pattern: ");
+        for (int i = 0; i < offset1; i++)
+            StdOut.print(" ");
+        StdOut.println(pat);
+
+        StdOut.print("pattern: ");
+        for (int i = 0; i < offset2; i++)
+            StdOut.print(" ");
+        StdOut.println(pat);
+        */
+    }
+}
---
+++ src/algrithm/sedgewick/strings/substringsearch/KMP.java
@@ -0,0 +1,116 @@
+package algrithm.sedgewick.strings.substringsearch;
+
+import algrithm.sedgewick.fundamental.programmodel.StdOut;
+
+public class KMP {
+    private final int R;       // the radix
+    private int[][] dfa;       // the KMP automation
+
+    private char[] pattern;    // either the character array for the pattern
+    private String pat;        // or the pattern string
+
+    // create the DFA from a String
+    public KMP(String pat) {
+        this.R = 256;
+        this.pat = pat;
+
+        // build DFA from pattern
+        int M = pat.length();
+        dfa = new int[R][M]; 
+        dfa[pat.charAt(0)][0] = 1; 
+        for (int X = 0, j = 1; j < M; j++) {
+            for (int c = 0; c < R; c++) 
+                dfa[c][j] = dfa[c][X];     // Copy mismatch cases. 
+            dfa[pat.charAt(j)][j] = j+1;   // Set match case. 
+            X = dfa[pat.charAt(j)][X];     // Update restart state. 
+        } 
+    } 
+
+    // create the DFA from a character array over R-character alphabet
+    public KMP(char[] pattern, int R) {
+        this.R = R;
+        this.pattern = new char[pattern.length];
+        for (int j = 0; j < pattern.length; j++)
+            this.pattern[j] = pattern[j];
+
+        // build DFA from pattern
+        int M = pattern.length;
+        dfa = new int[R][M]; 
+        dfa[pattern[0]][0] = 1; 
+        for (int X = 0, j = 1; j < M; j++) {
+            for (int c = 0; c < R; c++) 
+                dfa[c][j] = dfa[c][X];     // Copy mismatch cases. 
+            dfa[pattern[j]][j] = j+1;      // Set match case. 
+            X = dfa[pattern[j]][X];        // Update restart state. 
+        } 
+    } 
+
+    // return offset of first match; N if no match
+    public int search(String txt) {
+
+        // simulate operation of DFA on text
+        int M = pat.length();
+        int N = txt.length();
+        int i, j;
+        for (i = 0, j = 0; i < N && j < M; i++) {
+            j = dfa[txt.charAt(i)][j];
+        }
+        if (j == M) return i - M;    // found
+        return N;                    // not found
+    }
+
+
+    // return offset of first match; N if no match
+    public int search(char[] text) {
+
+        // simulate operation of DFA on text
+        int M = pattern.length;
+        int N = text.length;
+        int i, j;
+        for (i = 0, j = 0; i < N && j < M; i++) {
+            j = dfa[text[i]][j];
+        }
+        if (j == M) return i - M;    // found
+        return N;                    // not found
+    }
+
+    public static void test1(){
+    	String pat = "ABABAC";
+        String txt = "BCBAABACAABABACAA";
+        KMP kmp1 = new KMP(pat);
+        int offset1 = kmp1.search(txt);
+        System.out.println(txt);
+        for (int i = 0; i < offset1; i++)
+        	System.out.print(" ");
+        System.out.println(pat);
+    }
+
+    // test client
+    public static void main(String[] args) {
+    	test1();
+    	/*
+        String pat = args[0];
+        String txt = args[1];
+        char[] pattern = pat.toCharArray();
+        char[] text    = txt.toCharArray();
+
+        
+
+        KMP kmp2 = new KMP(pattern, 256);
+        int offset2 = kmp2.search(text);
+
+        // print results
+        StdOut.println("text:    " + txt);
+
+        StdOut.print("pattern: ");
+        
+            StdOut.print(" ");
+        StdOut.println(pat);
+
+        StdOut.print("pattern: ");
+        for (int i = 0; i < offset2; i++)
+            StdOut.print(" ");
+        StdOut.println(pat);
+        */
+    }
+}
